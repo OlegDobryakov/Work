c79ba65a1be6696db995f513d54c843d81b5ff04
Oleg Dobryakov <komhin3649@gmal.com>
2024-03-07 01:30:38 +0300

    Написали код для применение функции FillingArray

--- Program.cs
+++ Program.cs
@@ -1,7 +1,8 @@
-﻿string [] FillingArray(string [] array, int size)
-{   int characters = 0;
+﻿string[] FillingArray(string[] array, int size)
+{
+    int characters = 0;
     int w = 0;
-    string [] array2 = new string [size];
+    string[] array2 = new string[size];
     for (int i = 0; i < array.Length; i++)
     {
         for (int j = 0; j < array[i].Length; j++)
@@ -10,23 +11,26 @@
         }
         if (characters <= 3)
         {
-             array2[w] = array[i];
-             w++;
+            array2[w] = array[i];
+            w++;
 
         }
         characters = 0;
     }
     return array2;
 }
-
-
 System.Console.Write("Введите общее количество строк: ");
 int size = int.Parse(Console.ReadLine()!);
-string [] array = new string [size];
-string [] array2 = new string [size];
+string[] array = new string[size];
+string[] array2 = new string[size];
 System.Console.Write("Введите строки: ");
 for (int i = 0; i < array.Length; i++)
 {
-  string str = Console.ReadLine()!;
-    array[i] = str;  
+    string str = Console.ReadLine()!;
+    array[i] = str;
+}
+array2 = FillingArray(array, size);
+foreach (string e in array2)
+{
+    System.Console.Write(e + " ");
 }
