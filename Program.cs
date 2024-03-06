dffe6bba0a7f9f28aff3c20c4ad970c9a05ee275
Oleg Dobryakov <komhin3649@gmal.com>
2024-03-07 01:27:05 +0300

    Написали код для создания массива

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.Write("Введите общее количество строк: ");
+int size = int.Parse(Console.ReadLine()!);
+string [] array = new string [size];
+string [] array2 = new string [size];
+System.Console.Write("Введите строки: ");
+for (int i = 0; i < array.Length; i++)
+{
+  string str = Console.ReadLine()!;
+    array[i] = str;  
+}
