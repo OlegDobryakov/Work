058f502876e7bc4ab77ecc0f622d70ee255241fc
Oleg Dobryakov <komhin3649@gmal.com>
2024-03-07 01:28:19 +0300

    Написали код функции FillingArray

--- Program.cs
+++ Program.cs
@@ -1,4 +1,26 @@
-﻿System.Console.Write("Введите общее количество строк: ");
+﻿string [] FillingArray(string [] array, int size)
+{   int characters = 0;
+    int w = 0;
+    string [] array2 = new string [size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        for (int j = 0; j < array[i].Length; j++)
+        {
+            characters++;
+        }
+        if (characters <= 3)
+        {
+             array2[w] = array[i];
+             w++;
+
+        }
+        characters = 0;
+    }
+    return array2;
+}
+
+
+System.Console.Write("Введите общее количество строк: ");
 int size = int.Parse(Console.ReadLine()!);
 string [] array = new string [size];
 string [] array2 = new string [size];
